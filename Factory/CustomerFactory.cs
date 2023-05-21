09be5d8b8b4046e319b5daedb59e4f55779d4559
WunnaThaw <wunna.pe@gmail.com>
2023-05-21 20:45:55 +0630

    Factory Design Pattern

---
+++ Factory/CustomerFactory.cs
@@ -0,0 +1,40 @@
+﻿using CustomerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Factory
+{
+    public abstract class CustomerFactory
+    {
+        public ICustomer GetCustomer()
+        {
+            ICustomer myCust = this.CreateCustomer();
+            myCust.AddPoints();
+            myCust.AddDiscount();
+            return myCust;
+        }
+        public abstract ICustomer CreateCustomer();
+    }
+
+    public class GoldCustomerFactory : CustomerFactory
+    {
+        public override ICustomer CreateCustomer()
+        {
+            GoldCustomer objCust = new GoldCustomer();
+            objCust.GoldOperation();
+            return objCust;
+        }
+    }
+    public class SilverCustomerFactory : CustomerFactory
+    {
+        public override ICustomer CreateCustomer()
+        {
+            SilverCustomer objCust = new SilverCustomer();
+            objCust.SilverOperation();
+            return objCust;
+        }
+    }
+
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,23 @@
+﻿using CustomerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Client Code
+            CustomerFactory c = new GoldCustomerFactory();// Or new SilverCustomerFactory();
+            ICustomer objCust = c.GetCustomer();
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
